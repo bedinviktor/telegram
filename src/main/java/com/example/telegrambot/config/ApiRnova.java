8d73769c33670f72e1f297054ffeaaee8068aa42
bedin viktor <bedinviktor@yahoo.com>
2024-05-22 22:33:04 +0300

    Оптимизация импортов

--- src/main/java/com/example/telegrambot/config/ApiRnova.java
+++ src/main/java/com/example/telegrambot/config/ApiRnova.java
@@ -1,8 +1,6 @@
 package com.example.telegrambot.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,8 +12,6 @@ public class ApiRnova {
     private String host;
     private String api_key;
     public String hostEndpoints(String endpoint) {
-        ObjectMapper objectMapper = new ObjectMapper();
-
         return String.format("%s%s?api_key=%s", host, endpoint, api_key);
     }
 }
